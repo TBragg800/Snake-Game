ac8cb47f6f5c5f4b165c1cde0030388d60bb4306
LM&T <tbbragg83@gmail.com>
2020-12-01 01:48:57 -0600

    added primary functions/filling out Setup function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
 using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw()
+{
+// snake head controlls up, down, left, right
+// tail follows head
+// fruit increase tails
+// if the head hits the tail or the wall, you loose
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
 int main() 
 {
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10); sleep(10);
+    }
+    
 
     return 0;
 }

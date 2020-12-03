9599a13ca35d5543ed26ef4c5a02b85f9a8f7306
LM&T <tbbragg83@gmail.com>
2020-12-02 19:18:10 -0600

    adjusted code to work on ios

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
-#include <iostream>
-using namespace std;
+#include <cstdlib>
+#include <ncurses.h>
+
+
 bool gameOver;
-const int width = 20;
-const int height = 20;
-int x, y, fruitX, fruitY, score;
+const int width = 20, height = 20;
+int x, y, FruitX, FruitY, score;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
 
 void Setup()
 {
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    curs_set(0);
+
     gameOver = false;
     dir = STOP;
     x = width / 2;
     y = height / 2;
-    fruitX = rand() % width;
-    fruitY = rand() % height;
+    fruitX = (rand() % width) + 1;
+    fruitY = (rand() % height) + 1;
     score = 0;
 }
 
@@ -25,29 +32,33 @@ void Draw()
 // tail follows head
 // fruit increase tails
 // if the head hits the tail or the wall, you loose
-    system("clear");
-    for (int i = 0; i < width; i++) 
-        cout << "#";
-    cout << endl;
+    // system("clear");
+    // for (int i = 0; i < width; i++) 
+    //     cout << "#";
+    // cout << endl;
 
-    for (int i = 0; i < height + 2; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (j == 0)
-                cout << "#";
+    // for (int i = 0; i < height + 2; i++)
+    // {
+    //     for (int j = 0; j < width; j++)
+    //     {
+    //         if (j == 0)
+    //             cout << "#";
+    //         if (i == y && j == x)
+    //             cout << "O";
+    //         else if (i == fruitY && j == fruitX)
+    //             cout << "F";
+    //         else
+    //             cout << " ";
 
-                cout << " ";
-
-            if (j == width-1)
-                cout << "#";
-        }
-        cout << endl;
-    }
+    //         if (j == width-1)
+    //             cout << "#";
+    //     }
+    //     cout << endl;
+    // }
     
-    for (int i = 0; i < width + 2; i++) 
-        cout << "#";
-    cout << endl;
+    // for (int i = 0; i < width + 2; i++) 
+    //     cout << "#";
+    // cout << endl;
 }
 
 void Input()
@@ -73,4 +84,4 @@ int main()
     
 
     return 0;
-}
+// }
